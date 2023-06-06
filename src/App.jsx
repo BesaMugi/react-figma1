83210c127b25e45883e3da76c20eb4a53ef97818
BesaMugi <mugiwarabesa@mail.ru>
2023-06-06 10:08:44 +0300

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Footer from './components/Footer/Footer'
 const App = () => {
   return (
     <>
-          <Header/>
-          <MainContent/>
-          <Footer/>
+        <Header/>
+        <MainContent/>
+        <Footer/>
     </>
   )
 }
