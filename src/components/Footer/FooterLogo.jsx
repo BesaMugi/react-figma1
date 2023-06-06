5380b7989d1ee608a0b8e353daa8126fbd8fca68
BesaMugi <mugiwarabesa@mail.ru>
2023-06-06 10:50:28 +0300

    gg

--- src/components/Footer/FooterLogo.jsx
+++ src/components/Footer/FooterLogo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import footerLogo from "./logo2.png";
-import styles from "./Footer.module.css"
+// import styles from "./Footer.module.css"
 
 const FooterLogo = () => {
   return (
